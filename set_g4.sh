9013595f88577300a5fbdffaccc02b2265a5d557
Fernando Torales - Acosta <fernando@cori08.nersc.gov>
2021-03-11 15:17:20 -0800

    sets G4 variables

--- set_g4.sh
+++ set_g4.sh
@@ -1,13 +1,14 @@
 
-export G4LEVELGAMMADATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/PhotonEvaporation5.5
-export G4_MAIN=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02
-export G4LEDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/G4EMLOW7.9.1
-export G4NEUTRONHPDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/G4NDL4.6
-export G4ENSDFSTATEDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/G4ENSDFSTATE2.2
-export G4RADIOACTIVEDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/RadioactiveDecay5.4
-export G4ABLADATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/G4ABLA3.1
-export G4PIIDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/G4PII1.3
-export G4PARTICLEXSDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/G4PARTICLEXS2.1
-export G4SAIDXSDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/G4SAIDDATA2.0
-export G4REALSURFACEDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/RealSurface2.1.1
-export G4INCLDATA=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/geant4.10.06.p02/share/Geant4-10.6.2/data/G4INCL1.0
+export core_dir=/project/projectdirs/alice/ftorales/Singularity/cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core
+export G4LEVELGAMMADATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/PhotonEvaporation5.5
+export G4_MAIN=$core_dir/geant4.10.06.p02
+export G4LEDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/G4EMLOW7.9.1
+export G4NEUTRONHPDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/G4NDL4.6
+export G4ENSDFSTATEDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/G4ENSDFSTATE2.2
+export G4RADIOACTIVEDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/RadioactiveDecay5.4
+export G4ABLADATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/G4ABLA3.1
+export G4PIIDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/G4PII1.3
+export G4PARTICLEXSDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/G4PARTICLEXS2.1
+export G4SAIDXSDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/G4SAIDDATA2.0
+export G4REALSURFACEDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/RealSurface2.1.1
+export G4INCLDATA=$core_dir/geant4.10.06.p02/share/Geant4-10.6.2/data/G4INCL1.0
